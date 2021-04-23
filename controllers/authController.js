1068b838d0309d5ab97efbe9f9adc58197e7841a
prayogo bagus suntoro <prayogo0399@gmail.com>
2021-04-23 14:38:46 +0700

    add Login-API

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,73 +1,6 @@
 const { User } = require("../models");
 const Joi = require("joi");
-const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
-exports.registrasi = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const data = req.body;
-
-    const schema = Joi.object({
-      fullName: Joi.string().min(3).required(),
-      email: Joi.string().email.min(6).required(),
-      password: Joi.string().required(),
-    });
-
-    const { error } = schema.validate(req.body);
-
-    if (error) {
-      return res.send({
-        status: "Validation Failed",
-        message: error,
-      });
-    }
-
-    const checkEmail = await User.findOne({
-      where: {
-        email,
-      },
-    });
-
-    if (checkEmail) {
-      return res.send({
-        status: "Failed",
-        message: "Email already registered",
-      });
-    }
-
-    const secretKey = process.env.SECRET_KEY;
-    const token = jwt.sign(
-      {
-        id: email,
-      },
-      secretKey
-    );
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const dataUser = await User.create({
-      ...data,
-      password: hashedPassword,
-    });
-
-    res.status(200).send({
-      status: "Success",
-      data: {
-        user: {
-          fullName: dataUser.name,
-          token,
-        },
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({
-      status: "Failed",
-      message: "Server Error",
-    });
-  }
-};
 
 exports.login = async (req, res) => {
   try {
@@ -83,11 +16,11 @@ exports.login = async (req, res) => {
     if (error) {
       return res.send({
         status: "Validation failed",
-        message: error,
+        message: error.details[0].message,
       });
     }
 
-    const checkEmail = await user.findOne({
+    const checkEmail = await User.findOne({
       where: {
         email,
       },
@@ -95,16 +28,19 @@ exports.login = async (req, res) => {
     if (!checkEmail) {
       return res.send({
         status: "Login failed",
-        message: "Email/Password is wrong",
+        message: "Email/Password is wrong1",
       });
     }
 
-    const checkPassword = await bcrypt.compare(password, checkEmail.password);
-
+    const checkPassword = await User.findOne({
+      where: {
+        password,
+      },
+    });
     if (!checkPassword) {
       return res.send({
         status: "Login Failed",
-        message: "Email/Password is wrong",
+        message: "Email/Password is wrong2",
       });
     }
 
--- routers/index.js
+++ routers/index.js
@@ -2,20 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { auth } = require("../middleware/auth");
-const { upload } = require("../middleware/upload");
-
-const {
-  getUsers,
-  updateUser,
-  deleteUser,
-} = require("../controllers/UserController");
-router.get("/users", auth, getUsers);
-router.patch("/user", auth, upload("imageFile"), updateUser);
-router.delete("/user/:id", auth, deleteUser);
-
-const { registrasi, login } = require("../controllers/authController");
-router.post("/register", registrasi);
+const { login } = require("../controllers/authController");
 router.post("/login", login);
 
 module.exports = router;
